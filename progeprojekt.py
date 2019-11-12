0724abf432cddb3b5afebae095bcf37a5173fd85
erikaalexandramilani <56923834+erikaalexandramilani@users.noreply.github.com>
2019-11-12 19:57:41 +0200

    Add files via upload

---
+++ progeprojekt.py
@@ -0,0 +1,32 @@
+import pygame
+pygame.init()
+ 
+size = (800, 800)
+screen = pygame.display.set_mode(size)
+ 
+pygame.display.set_caption("Seiklus Donaldiga")
+ 
+# tsükklis kuni kinni pannakse
+done = False
+ 
+#kui kiiresti pilt uuendatakse?
+clock = pygame.time.Clock()
+background_image = pygame.image.load("square-starfield.jpg").convert()
+pygame.mixer.music.load("senorita.mp3")
+pygame.mixer.music.play()
+tick(0)
+ 
+# terve mängu tsükkel
+while not done:
+    # programm ise
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+#mängu loogika siia:
+    screen.blit(background_image, [0,0])
+    
+    pygame.display.flip()
+ 
+    clock.tick(60)
+ 
+pygame.quit()
